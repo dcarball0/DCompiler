5b8bf76718376191fdde6b04ef2c9d35b4ace5af
SeiGaX <seigaxxgamer@hotmail.com>
2023-03-16 17:49:36 +0100

    Removed useless functions bintree

--- binTree.c
+++ binTree.c
@@ -66,53 +66,12 @@ binTree treeL(binTree A) {
 binTree treeR(binTree A) {
     return A->treeR;
 }
-
-void treeRead(binTree A, lexComp *E) {
-    *E = A->info;
-}
 // Función privada para comparar las claves
-
 int _treeCompareKey(lexCompKey cl, lexComp E) {
     return treeCompareKeys(cl, treeGetNodeKey(&E));
 }
-//Función privada para informar si una clave está en el árbol
-
-unsigned treeIsMemberByKey(binTree A, lexCompKey cl) {
-    if (treeIsEmpty(A)) {
-        return 0;
-    }
-    int comp = _treeCompareKey(cl, A->info);
-
-    if (comp == 0) { //cl == A->info
-        return 1;
-    }
-    if (comp > 0) { //cl > A->info
-        return treeIsMemberByKey(treeR(A), cl);
-    }
-    //cl < A->info
-    return treeIsMemberByKey(treeL(A), cl);
-}
 
 //Funciones públicas
-
-unsigned treeIsMember(binTree A, lexComp E) {
-    return treeIsMemberByKey(A, treeGetNodeKey(&E));
-}
-
-void treeNodeSearch(binTree A, lexCompKey cl, lexComp *nodo) {
-    if (treeIsEmpty(A)) {
-        return;
-    }
-    int comp = _treeCompareKey(cl, A->info);
-
-    if (comp == 0) { // cl == A->info
-        *nodo = A->info;
-    } else if (comp < 0) { // cl < A->info
-        treeNodeSearch(A->treeL, cl, nodo);
-    } else { // cl > A->info
-        treeNodeSearch(A->treeR, cl, nodo);
-    }
-}
 //OPERACIONES DE MODIFICACIÓN
 
 /* Funcion recursiva para insertar un nuevo nodo 
--- binTree.h
+++ binTree.h
@@ -42,28 +42,6 @@ binTree treeL(binTree A);
  * @param A - Arbol original
  */
 binTree treeR(binTree A);
-/**
- * Recupera la informacion de la celda de la raiz del arbol
- * @param A
- * @param E Puntero al nodo que apuntara a la informacion
- */
-void treeRead(binTree A, lexComp *E);
-
-/**
- * Comprueba si el elemento <E> existe en el arbol <A>
- * @param A
- * @param E
- */
-unsigned treeIsMember(binTree A, lexComp E);
-
-/**
- * Busca un nodo con clave <cl> en el arbol y, si existe
- * modifica el puntero <nodo> para que lo apunte.
- * @param A Arbol binario en el que se busca el nodo.
- * @param cl Clave que se buscara.
- * @param nodo Puntero a un lexComp.
- */
-void treeNodeSearch(binTree A, lexCompKey cl, lexComp *nodo);
 
 //FUNCIONES DE MODIFICACIÓN
 /**
